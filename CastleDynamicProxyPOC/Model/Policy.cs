31e8ff5733946e6a1b6eec5b78464c2bb12d27f8
jasallen <github@deboever.us>
2012-02-16 15:50:00 -0500

    initial commit

---
+++ CastleDynamicProxyPOC/Model/Policy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CastleDynamicProxyPOC.Model
+{
+	public class Policy
+	{
+		public Policy()
+		{
+			AccountNumber = "ORIGINAL ACCOUNT";
+			Coverages = new List<Coverage>();	
+		}
+
+		
+		public virtual String AccountNumber { get; set; }
+
+		public virtual List<Coverage> Coverages { get; set; }
+
+		public void touch()
+		{
+			Console.WriteLine("touched.");
+		}
+	}
+}
---
+++ CastleDynamicProxyPOC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CastleDynamicProxyPOC.Model;
+
+namespace CastleDynamicProxyPOC
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+	
+			//intercepted
+			var policy2 = ShadowedObject.Create<Policy>();
+			policy2.AccountNumber = "987xyz";
+			policy2.AccountNumber = "456lmn";
+			policy2.touch();
+
+			policy2.ResetToOriginal("AccountNumber");
+
+			//policy2.Coverages.Add(new Coverage());
+			//policy2.Coverages[0].name = "ChangedVal";
+
+			//policy2.ResetToOriginal("Coverages");
+
+			Console.ReadLine();
+		}
+	}
+}
